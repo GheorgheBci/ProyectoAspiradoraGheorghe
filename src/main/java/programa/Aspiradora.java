4826d7b1cdc75fe453894d18d9fc14b8d9a91a19
Gheorghe <georgebucu96@gmail.com>
2020-11-04 17:33:16 +0100

    Crea el menu del programa

--- src/main/java/programa/Aspiradora.java
+++ src/main/java/programa/Aspiradora.java
@@ -1,12 +1,25 @@
 package programa;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Jorge
  */
 public class Aspiradora {
-    
+
     public static void main(String[] args) {
-        
-    }   
+
+        int opciones;
+
+        JOptionPane.showMessageDialog(null, "Bienvenido al menú de la aspiradora");
+        opciones = Integer.parseInt(JOptionPane.showInputDialog("¿Qué opción desea ver? \n"
+                + "1- Configurar el sistema \n"
+                + "2- Carga \n"
+                + "3- Aspiración \n"
+                + "4- Aspiración y fregado \n"
+                + "5- Estado general \n"
+                + "6- Base de carga \n"
+                + "7- Salir"));
+    }
 }
