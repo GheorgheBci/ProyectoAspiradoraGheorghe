485e1591d2563256217405601930c04e3acb4218
Gheorghe <georgebucu96@gmail.com>
2020-11-15 16:54:32 +0100

    Implementa la opción de aspiración en modo dependencias

--- src/main/java/programa/Aspiradora.java
+++ src/main/java/programa/Aspiradora.java
@@ -7,14 +7,15 @@ import javax.swing.JOptionPane;
  * @author Jorge
  */
 public class Aspiradora {
-    
+
     public static void main(String[] args) {
-        
+
         final String USUARIO = "George";
         final int CONTRASENIA = 12345;
-        
+
         int opciones, salir;
         int opcionesAspiracion;
+        int opcionesDependencias;
         boolean repetir = true;
         String usuarioCorrecto;
         int contraseniaCorrecto;
@@ -22,40 +23,40 @@ public class Aspiradora {
         String dependencia = "";
         int contador = 0;
         double carga = 0;
-        
+
         double[] metrosDepen = new double[5];
-        
+
         String[] dependencias = new String[5];
         dependencias[0] = "la cocina";
         dependencias[1] = "el salón";
         dependencias[2] = "el cuarto de baño";
         dependencias[3] = "el primer dormitorio";
         dependencias[4] = "el segundo dormitorio";
-        
+
         JOptionPane.showMessageDialog(null, "Bienvenido al menú de la aspiradora");
         usuarioCorrecto = JOptionPane.showInputDialog("Indica el usuario");
-        
+
         while (!usuarioCorrecto.equals(USUARIO)) {
             usuarioCorrecto = JOptionPane.showInputDialog("Usuario incorrecto, prueba de nuevo");
         }
-        
+
         contraseniaCorrecto = Integer.parseInt(JOptionPane.showInputDialog("Indica la contraseña"));
-        
+
         while (contraseniaCorrecto != CONTRASENIA) {
             contraseniaCorrecto = Integer.parseInt(JOptionPane.showInputDialog("Contraseña incorrecta, prueba de nuevo"));
         }
-        
+
         JOptionPane.showMessageDialog(null, "Has accedido con exito al programa");
         JOptionPane.showMessageDialog(null, "Primero vamos a indicar cuantos metros tiene cada sala de la vivienda");
-        
+
         for (int i = 0; i < metrosDepen.length; i++) {
             do {
                 metrosDepen[i] = Double.parseDouble(JOptionPane.showInputDialog("Cuantos metros tiene " + dependencias[i]));
             } while (metrosDepen[i] < 1 || metrosDepen[i] > 100);
         }
-        
+
         do {
-            
+
             do {
                 opciones = Integer.parseInt(JOptionPane.showInputDialog("¿Qué opción desea ver? \n"
                         + "1- Carga \n"
@@ -65,49 +66,47 @@ public class Aspiradora {
                         + "5- Base de carga \n"
                         + "6- Salir"));
             } while (opciones < 1 || opciones > 6);
-            
+
             switch (opciones) {
                 case 1:
                     JOptionPane.showMessageDialog(null, "Has elegido la opción de carga");
-                    
+
                     do {
                         nivelBateria = Integer.parseInt(JOptionPane.showInputDialog("Indica el nivel de batería que tiene la aspiradora"));
                     } while (nivelBateria < 0 || nivelBateria > 100);
                     break;
-                
+
                 case 2:
                     JOptionPane.showMessageDialog(null, "Has elegido la opción de aspiración");
-                    
+
                     do {
                         opcionesAspiracion = Integer.parseInt(JOptionPane.showInputDialog("¿Qué opción desea hacer? \n"
                                 + "1- Modo completo \n"
                                 + "2- Modo dependencias \n"));
                     } while (opcionesAspiracion < 1 || opcionesAspiracion > 2);
-                    
+
                     switch (opcionesAspiracion) {
                         case 1:
                             JOptionPane.showMessageDialog(null, "Modo completo");
-                            
+
                             for (int i = 0; i < metrosDepen.length; i++) {
                                 carga = (1.5 * metrosDepen[i]) / 1;
                                 nivelBateria = nivelBateria - carga;
-                                
+
                                 if (nivelBateria < 3) {
                                     JOptionPane.showMessageDialog(null, "La aspiradora no tiene suficiente batería para limpiar y esta "
                                             + "parado en " + dependencias[i] + ", por favor cargue la aspiradora en la base de carga");
-                                    
+
                                     for (int j = 0; j != contador; j++) {
-                                        
+
                                         try {
                                             JOptionPane.showMessageDialog(null, "La aspiradora ha limpiado " + dependencias[j]);
                                         } catch (ArrayIndexOutOfBoundsException aiooe) {
-                                            
                                             JOptionPane.showMessageDialog(null, "La aspiradora ha limpiado todas las habitaciones y le queda de batería"
                                                     + " un " + (nivelBateria = Math.abs(nivelBateria)) + "%");
-                                            break;
                                         }
                                     }
-                                    
+
                                     if (contador == 0) {
                                         JOptionPane.showMessageDialog(null, "La aspiradora no ha limpiado ninguna habitación");
                                     }
@@ -115,33 +114,82 @@ public class Aspiradora {
                                 }
                                 contador++;
                             }
-                            
+
                             if (nivelBateria > 3) {
-                                
+
                                 if (nivelBateria < 0) {
                                     nivelBateria = Math.abs(nivelBateria);
                                 }
-                                
+
                                 JOptionPane.showMessageDialog(null, "La aspiradora ha limpiado todas las habitaciones y le queda de batería"
                                         + " un " + nivelBateria + "%");
                             }
                             break;
-                        
+
                         case 2:
+                            JOptionPane.showMessageDialog(null, "Modo dependencias");
+
+                            do {
+                                opcionesDependencias = Integer.parseInt(JOptionPane.showInputDialog("¿Qué dependencia quieres limpiar? \n"
+                                        + "0- La cocina \n"
+                                        + "1- El salón \n"
+                                        + "2- El cuarto de baño \n"
+                                        + "3- El primer dormitorio \n"
+                                        + "4- El segundo dormitorio"));
+                            } while (opcionesDependencias < 0 || opcionesDependencias > 4);
+
+                            switch (opcionesDependencias) {
+                                case 0:
+                                    dependencia = "la cocina";
+                                    carga = (1.5 * metrosDepen[0]) / 1;
+                                    nivelBateria = nivelBateria - carga;
+                                    break;
+
+                                case 1:
+                                    dependencia = "el salón";
+                                    carga = (1.5 * metrosDepen[1]) / 1;
+                                    nivelBateria = nivelBateria - carga;
+                                    break;
+
+                                case 2:
+                                    dependencia = "el cuarto de baño";
+                                    carga = (1.5 * metrosDepen[2]) / 1;
+                                    nivelBateria = nivelBateria - carga;
+                                    break;
+
+                                case 3:
+                                    dependencia = "el primer dormitorio";
+                                    carga = (1.5 * metrosDepen[3]) / 1;
+                                    nivelBateria = nivelBateria - carga;
+                                    break;
+
+                                case 4:
+                                    dependencia = "el segundo dormitorio";
+                                    carga = (1.5 * metrosDepen[4]) / 1;
+                                    nivelBateria = nivelBateria - carga;
+                                    break;
+                            }
+
+                            if (nivelBateria < 3) {
+                                JOptionPane.showMessageDialog(null, "La aspiradora no tiene suficiente batería para limpiar " + dependencia
+                                        + ", por favor ve a la base de carga");
+                            } else {
+                                JOptionPane.showMessageDialog(null, "La aspiradora ha limpiado " + dependencia);
+                            }
                             break;
                     }
                     break;
-                
+
                 case 3:
                     JOptionPane.showMessageDialog(null, "Has elegido la opción de aspiración y fregado");
                     break;
                 case 4:
                     JOptionPane.showMessageDialog(null, "Has elegido la opción de estado general");
                     break;
-                
+
                 case 5:
                     JOptionPane.showMessageDialog(null, "Has elegido la opción de base de carga");
-                    
+
                     if (nivelBateria == 100) {
                         JOptionPane.showMessageDialog(null, "La batería ya está al 100%");
                     } else {
@@ -149,11 +197,11 @@ public class Aspiradora {
                     }
                     nivelBateria = 100;
                     break;
-                
+
                 case 6:
                     salir = JOptionPane.showConfirmDialog(null,
                             "¿Deseas salir?", "Salida del programa", JOptionPane.YES_NO_OPTION);
-                    
+
                     if (salir == JOptionPane.YES_OPTION) {
                         JOptionPane.showMessageDialog(null, "Saliendo con exito");
                         repetir = false;
